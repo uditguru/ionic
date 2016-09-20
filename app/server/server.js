15cc2b9bcfd072867f0adee3a447b4d2d6db1792
Udit <guru.udit@gmail.com>
2016-09-20 14:45:06 +0530

    some:s121

--- app/server/server.js
+++ app/server/server.js
@@ -1,134 +1,111 @@
-// Set up
-var passport = require('passport');
-var express  = require('express');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var cors = require('cors');
-var jwt = require('jsonwebtoken');
-var router = require('../routes/routes');
+var CONTACTS_COLLECTION = "contacts";
 
-// Configuration
-mongoose.connect('mongodb://localhost/Goserv');
-var app      = express();
-app.use(express.static('public'));
+var app = express();
+app.use(express.static(__dirname + 'public'));
+app.use(bodyParser.json());
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-app.use(cors());
-app.use(passport.initialize());
-require('../config/passport')(passport);
-var requireAuth = passport.authenticate('local', { session: false });
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-app.use(router);
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
-
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
   });
 });
 
-    var Schema = mongoose.Schema;
+// CONTACTS API ROUTES BELOW
 
-        var schema = new Schema({
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
-        type: String,
-        address: String,
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
 
-        servicecost: String
-
-    }, { collection: 'Goserv' });
-
-
-
-
-
-    var Goserv = mongoose.model('Goserv', schema  );
-
-     module.exports = Goserv ;
-
-
-        app.get('/api/centre', function(req, res) {
-
-
-            Goserv.find({},function(err, centre){
-                if(err){
-                    res.send(err);
-               } else {
-                    res.json(centre);
-                    console.log(centre);
-               }
-
-            });
-
-        });
-
-
-    app.post('/adduser', function(req, res) {
-    if(!req.body.name || !req.body.password) {
-    res.json({ success: false, message: 'Please enter name and password.' });
+app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
     } else {
-    var newUser = new User({
-      name: req.body.name,
-      password: req.body.password
-    });
+      res.status(200).json(docs);
+    }
+  });
+});
 
-    // Attempt to save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({ success: false, message: 'That name address already exists.'});
-      }
-      res.json({ success: true, message: 'Successfully created new user.' });
-        });
-      }
-    });
+app.post("/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
 
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
 
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
 
-        app.post('/dashboard',requireAuth, function(req, res){
-             res.send({message: req.user})
-                        });
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
 
+app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
 
+app.put("/contacts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
 
-        app.post('/api/centre/request', function(req, res) {
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
 
-        console.log(req.body._id);
-
-        Centre.findByIdAndUpdate(req.body._id, {
-            $push: {"reserved": {address: req.body.address}}
-        }, {
-            safe: true,
-            new: true
-        }, function(err, centre){
-            if(err){
-                res.send(err);
-            } else {
-                res.json(centre);
-            }
-        });
-
-        });
-
-// listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
+app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
